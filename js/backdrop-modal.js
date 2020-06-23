3c5148c816095b122b9b555ecce76b52984a9238
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-06-24 01:40:03 +0300

    Добавляет Mobile-First
    Все для мобильного кроме футера, фотографий членов команды. Для таблетки сделал немного (первые секции)

---
+++ js/backdrop-modal.js
@@ -0,0 +1,31 @@
+// скрипт для добавления класса модальному окну
+const refs = {
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+refs.backdrop.addEventListener('click', logBackdropClick);
+
+function toggleModal() {
+  refs.backdrop.classList.toggle('is-hidden');
+}
+
+function logBackdropClick() {
+  console.log('Это клик в бекдроп');
+}
+// скрипт для отправки данных через форму в нашу консоль
+(() => {
+  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`),
+    );
+
+    e.currentTarget.reset();
+  });
+})();
